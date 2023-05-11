957c614a2663d5f19ffc005e192ebb7a203d903d
Suman <35394894+sumangeo@users.noreply.github.com>
2023-05-11 17:14:13 +0600

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -5,12 +5,27 @@ class MyDocument extends Document {
   render() {
     return (
       <Html lang="en" className="theme-compiled">
+       <Head>
+      {/* Netlify Widget */}
+          <script async src="https://identity.netlify.com/v1/netlify-identity-widget.js" />
         <Head />
         <body
           className={`antialiased text-lg bg-white dark:bg-gray-900 dark:text-white leading-base`}
         >
           <Main />
           <NextScript />
+            <script dangerouslySetInnerHTML={{
+            __html: `
+              if (window.netlifyIdentity) {
+                window.netlifyIdentity.on("init", user => {
+                  if (!user) {
+                    window.netlifyIdentity.on("login", () => {
+                      document.location.href = "/admin/";
+                    });
+                  }
+                });
+              }
+          `}}/>
         </body>
       </Html>
     );
